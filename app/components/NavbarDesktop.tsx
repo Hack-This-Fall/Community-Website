734b93141b0085fb0f89c8bc92cf71fa50d29b32
Apoorv Dwivedi <apoorvd14@gmail.com>
2023-10-14 16:13:57 +0530

    navbar refactored

--- app/components/NavbarDesktop.tsx
+++ app/components/NavbarDesktop.tsx
@@ -1,78 +1,98 @@
-import { Image } from "@chakra-ui/react";
+import { Box, Flex, Image, Text, Link } from '@chakra-ui/react';
 
-
-const NavbarDesktop = () => {
-    return (
-      <header>
-        <nav className="bg-white border-gray-200 px-4 lg:px-6 py-2.5">
-          <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
-            <a href="" className="flex items-center">
-              <Image
-                src="/logo.png"
-                className="mr-3 h-6 sm:h-9"
-                alt="HackThisFall Logo"
-              />
-            </a>
-            <div
-              className="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1"
-              id="mobile-menu-2"
+const Header = () => {
+  return (
+    <Box as="header">
+      <Flex
+        className="bg-white border-gray-200 px-4 lg:px-6 py-2.5"
+        align="center"
+        justify="space-between"
+        maxW="screen-xl"
+        mx="auto"
+      >
+        <Link href="">
+          <Image
+            src="/logo.png"
+            mr={3}
+            h={['6', '9']}
+            alt="HackThisFall Logo"
+          />
+        </Link>
+        <Flex
+          className="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1"
+          id="mobile-menu-2"
+        >
+          <Flex
+            direction={['column', 'row']}
+            mt={[4, 0]}
+            fontWeight="medium"
+            rowGap={8}
+          >
+            <Link
+              href="#"
+              py={2}
+              pr={4}
+              pl={3}
+              color="#676767"
+              _hover={{ color: 'black' }}
             >
-              <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
-                <li>
-                  <a
-                    href="#"
-                    className="block py-2 pr-4 pl-3 text-[#676767] rounded bg-primary-700 lg:bg-transparent lg:text-primary-700 lg:p-0 dark:text-white"
-                    aria-current="page"
-                  >
-                    Home
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="#"
-                    className="block py-2 pr-4 pl-3 text-[#676767] border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
-                  >
-                    Events
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="#"
-                    className="block py-2 pr-4 pl-3 text-[#676767] border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
-                  >
-                    Blogs
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="#"
-                    className="block py-2 pr-4 pl-3 text-[#676767] border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
-                  >
-                    Team
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="#"
-                    className="block py-2 pr-4 pl-3 text-[#676767] border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
-                  >
-                    Swag
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="#"
-                    className="block py-2 pr-4 pl-3 text-[#676767] border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
-                  >
-                    Contact us
-                  </a>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </nav>
-      </header>
-    );
-}
+              Home
+            </Link>
+            <Link
+              href="#"
+              py={2}
+              pr={4}
+              pl={3}
+              color="#676767"
+              _hover={{ color: 'black' }}
+            >
+              Events
+            </Link>
+            <Link
+              href="#"
+              py={2}
+              pr={4}
+              pl={3}
+              color="#676767"
+              _hover={{ color: 'black' }}
+            >
+              Blogs
+            </Link>
+            <Link
+              href="#"
+              py={2}
+              pr={4}
+              pl={3}
+              color="#676767"
+              _hover={{ color: 'black' }}
+            >
+              Team
+            </Link>
+            <Link
+              href="#"
+              py={2}
+              pr={4}
+              pl={3}
+              color="#676767"
+              _hover={{ color: 'black' }}
+            >
+              Swag
+            </Link>
+            <Link
+              href="#"
+              py={2}
+              pr={4}
+              pl={3}
+              color="#676767"
+              _hover={{ color: 'black' }}
+            >
+              Contact us
+            </Link>
+          </Flex>
+        </Flex>
+      </Flex>
+    </Box>
+  );
+};
 
-export default NavbarDesktop;
+export default Header;
