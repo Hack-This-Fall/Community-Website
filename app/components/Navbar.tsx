1cc4e7ab9272152f22cb94f297441772b01278cf
Paras Gupta <parasg1999@gmail.com>
2024-06-29 20:59:27 +0530

    wip: navbar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,8 +1,10 @@
 import Image from "next/image";
 import Logo from "../assets/images/logo.png";
 import Burger from "../assets/images/navbar/burger.svg";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useWindowSize } from "rooks";
+
+const navbarElementsDefault = ["Home", "About", "Events", "Blog"];
 
 export default function Navbar({
   setNavbarHeight,
@@ -13,6 +15,10 @@ export default function Navbar({
 }) {
   const ref = useRef<any>();
   const navbarLogo = useRef<any>();
+
+  const [navbarElements, setNavbarElements] = useState(navbarElementsDefault);
+
+  const [isNavbarOpen, setNavbarOpen] = useState(true);
 
   useEffect(() => {
     setNavbarHeight(ref.current?.clientHeight);
@@ -26,8 +32,6 @@ export default function Navbar({
     if (!height) return;
 
     const position = window.scrollY;
-
-    // console.log(position, height);
 
     if (position >= height) {
       navbarLogo.current.style.opacity = "0";
@@ -48,37 +52,52 @@ export default function Navbar({
     };
   }, []);
 
+  const removeElement = () => {
+    const newNavbarElements = [...navbarElements];
+    newNavbarElements.pop();
+    setNavbarElements(newNavbarElements);
+  };
+
   return (
     <div
+      className="sticky container-1200 py-4 md:py-8 absolute top-0 md:top-0 z-50  bg-white"
       ref={ref}
-      className={`flex justify-between items-center py-4 md:py-8 z-50 block w-full absolute top-0 md:top-0 sticky container-1200`}
     >
-      <Image
-        ref={navbarLogo}
-        src={Logo}
-        className="w-20 md:w-[7.5rem]"
-        alt="logo"
-      />
-      <div className="flex gap-x-12 font-outfit justify-between items-center font-regular text-xl mix-blend-exclusion	">
-        <span className="text-black pointer-events-auto cursor-pointer hidden md:inline-block">
-          Home
-        </span>
-        <span className="text-black pointer-events-auto cursor-pointer hidden md:inline-block">
-          About
-        </span>
-        <span className="text-black pointer-events-auto cursor-pointer hidden md:inline-block">
-          Events
-        </span>
-        <span className="text-black pointer-events-auto cursor-pointer hidden md:inline-block">
-          Blog
-        </span>
+      <div className={`flex justify-between items-center block w-full `}>
+        <Image
+          ref={navbarLogo}
+          src={Logo}
+          className="w-20 md:w-[7.5rem]"
+          alt="logo"
+        />
+        <div
+          className={
+            `flex gap-x-12 font-outfit justify-between items-center font-regular text-xl border-[0.5px] border-black px-6 py-4 rounded-full transition-all duration-200  
+                ease-in-out ` + isNavbarOpen
+              ? "w-full"
+              : "w-20"
+          }
+        >
+          {isNavbarOpen &&
+            navbarElements.map((element, index) => (
+              <span className="text-black pointer-events-auto cursor-pointer hidden md:inline-block">
+                {element}
+              </span>
+            ))}
+          <span
+            className="text-black pointer-events-auto cursor-pointer hidden md:inline-block"
+            onClick={removeElement}
+          >
+            X
+          </span>
 
-        {/* <span
+          {/* <span
           className="bg-black ml-0 md:ml-4 py-2 px-6 rounded-full text-white pointer-events-auto cursor-pointer"
           onClick={() => setIsNavbarOpen(true)}
         >
           <Image src={Burger} className="w-5 md:w-10" alt="logo" />
         </span> */}
+        </div>
       </div>
     </div>
   );
