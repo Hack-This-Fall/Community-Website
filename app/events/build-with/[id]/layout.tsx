c0803a369e8f9f65410b621319d36ed77865835a
Apoorv Dwivedi <apoorvd14@gmail.com>
2025-01-09 22:50:41 +0530

    added generateMetadata for build with

---
+++ app/events/build-with/[id]/layout.tsx
@@ -0,0 +1,91 @@
+import { Metadata } from 'next';
+import eventsData from '../../data';
+import { redirect } from 'next/navigation';
+
+export async function generateMetadata({
+  params,
+}: {
+  params: { id: string };
+}): Promise<Metadata> {
+  const eventId = params.id;
+  const eventData = eventsData.events.find(
+    (event) => event.id === eventId && event.individualPageActive,
+  );
+
+  return eventData
+    ? {
+        title: `${eventData?.heading} | Hack This Fall Community`,
+        openGraph: {
+          title: `${eventData?.heading} | Hack This Fall Community`,
+          description: eventData?.partnerDescription,
+          siteName: `${eventData?.heading} | Hack This Fall Community`,
+          images: [
+            {
+              url: `https://hackthisfall.tech/${eventData?.heroImage}`,
+              width: 2000,
+              height: 1000,
+              alt: 'Hack This Fall Community',
+            },
+          ],
+        },
+        twitter: {
+          title: `${eventData?.heading} | Hack This Fall Community`,
+          description: eventData?.partnerDescription,
+          images: [
+            {
+              url: `https://hackthisfall.tech/${eventData?.heroImage}`,
+              width: 2000,
+              height: 1000,
+              alt: 'Hack This Fall Community',
+            },
+          ],
+        },
+      }
+    : {
+        title: 'Events | Hack This Fall Community',
+        openGraph: {
+          title: 'Events | Hack This Fall Community',
+          description:
+            "Hack This Fall is one of India's most thriving hacker communities. Join us to build groundbreaking solutions, inspire problem-solvers, and grow together in a diverse, supportive environment",
+          siteName: 'Events | Hack This Fall Community',
+          images: [
+            {
+              url: 'https://hackthisfall.tech/og.png',
+              width: 2000,
+              height: 1000,
+              alt: 'Hack This Fall Community',
+            },
+          ],
+        },
+        twitter: {
+          title: 'Events | Hack This Fall Community',
+          images: [
+            {
+              url: 'https://hackthisfall.tech/og.png',
+              width: 2000,
+              height: 1000,
+              alt: 'Hack This Fall Community',
+            },
+          ],
+          description:
+            "Hack This Fall is one of India's most thriving hacker communities. Join us to build groundbreaking solutions, inspire problem-solvers, and grow together in a diverse, supportive environment",
+        },
+      };
+}
+
+export default function Layout({
+  children,
+  params,
+}: {
+  children: React.ReactNode;
+  params: { id: string };
+}) {
+  const eventId = params.id;
+  const eventData = eventsData.events.find(
+    (event) => event.id === eventId && event.individualPageActive,
+  );
+  if (!eventData) {
+    redirect('/404');
+  }
+  return children;
+}
--- app/events/layout.tsx
+++ app/events/layout.tsx
@@ -8,9 +8,25 @@ export const metadata: Metadata = {
     description:
       "Hack This Fall is one of India's most thriving hacker communities. Join us to build groundbreaking solutions, inspire problem-solvers, and grow together in a diverse, supportive environment",
     siteName: 'Events | Hack This Fall Community',
+    images: [
+      {
+        url: 'https://hackthisfall.tech/og.png',
+        width: 2000,
+        height: 1000,
+        alt: 'Hack This Fall Community',
+      },
+    ],
   },
   twitter: {
     title: 'Events | Hack This Fall Community',
+    images: [
+      {
+        url: 'https://hackthisfall.tech/og.png',
+        width: 2000,
+        height: 1000,
+        alt: 'Hack This Fall Community',
+      },
+    ],
     description:
       "Hack This Fall is one of India's most thriving hacker communities. Join us to build groundbreaking solutions, inspire problem-solvers, and grow together in a diverse, supportive environment",
   },
